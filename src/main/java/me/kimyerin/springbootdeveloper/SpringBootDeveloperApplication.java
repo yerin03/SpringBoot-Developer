f36960ddd53d00c6c56b7c5a9d61d522c36441d9
yerin03 <kyr2532@naver.com>
2023-06-28 15:54:18 +0900

    feat: TestController작성

--- src/main/java/me/kimyerin/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/kimyerin/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -5,10 +5,12 @@ package me.kimyerin.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //spring boot에 필요한 기본 설정을 해준다.
+
 public class SpringBootDeveloperApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    public static void main(String[] args) { //자바의 main() 메서드 같은 역할 , 여기서 스프링 부트 시작
+        SpringApplication.run(SpringBootDeveloperApplication.class, args); //첫번째 인수 : 메인 클래스 ,
+                                                                           //두번째 인수 : 커맨드 라인의 인수들을 전달
     }
 }
 
---
+++ src/main/java/me/kimyerin/springbootdeveloper/TestController.java
@@ -0,0 +1,14 @@
+package me.kimyerin.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello, World";
+    }
+}
