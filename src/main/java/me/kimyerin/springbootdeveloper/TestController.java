b83f4641e928f80aec8f37c2e32c7cc23580068c
yerin03 <kyr2532@naver.com>
2023-06-28 16:59:34 +0900

    feat: TestService 작성

--- src/main/java/me/kimyerin/springbootdeveloper/TestController.java
+++ src/main/java/me/kimyerin/springbootdeveloper/TestController.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import java.lang.reflect.Member;
+import java.util.List;
+
 @RestController
 public class TestController {
 
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test") //라우터 역할: http 요청과 메서드를 연결하는 장치
-    public String test(){
-        return "Hello, World";
+    public List<Member> getAllMembers(){
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
