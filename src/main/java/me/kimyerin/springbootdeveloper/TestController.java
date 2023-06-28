c26ecfcf1b9cc2778f5bfb93586e4a921e4435ea
yerin03 <kyr2532@naver.com>
2023-06-28 16:47:58 +0900

    build: jpa,H2,lombok 의존성 추가

--- src/main/java/me/kimyerin/springbootdeveloper/TestController.java
+++ src/main/java/me/kimyerin/springbootdeveloper/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/test") //라우터 역할: http 요청과 메서드를 연결하는 장치
     public String test(){
         return "Hello, World";
     }
