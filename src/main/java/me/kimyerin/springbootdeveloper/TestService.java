a949229b0287eebec6b64b00b5a98267b96c2e0e
yerin03 <kyr2532@naver.com>
2023-06-28 18:33:18 +0900

    fix: 오타수정

--- src/main/java/me/kimyerin/springbootdeveloper/TestService.java
+++ src/main/java/me/kimyerin/springbootdeveloper/TestService.java
@@ -7,12 +7,10 @@ import java.util.List;
 
 @Service
 public class TestService {
-
     @Autowired
     MemberRepository memberRepository;
 
     public List<Member> getAllMembers() {
-        return memberRepository.findAll()
-
+        return memberRepository.findAll();
     }
 }
