a0549f2c7a9364dba338d1f5e64900c2dc151f01
yerin03 <kyr2532@naver.com>
2023-06-28 13:56:11 +0900

    init: 프로젝트 환경 세팅

---
+++ src/main/java/me/kimyerin/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,15 @@
+
+
+package me.kimyerin.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
+
+
