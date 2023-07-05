3858e7d128f5c0a49f5bcf23e931ac27c19ef812
yerin03 <kyr2532@naver.com>
2023-07-05 10:08:52 +0900

    feat: article controller

---
+++ src/main/java/me/kimyerin/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,30 @@
+package me.kimyerin.springbootdeveloper.controller;
+
+import jakarta.persistence.Id;
+import lombok.RequiredArgsConstructor;
+import me.kimyerin.springbootdeveloper.domain.Article;
+import me.kimyerin.springbootdeveloper.dto.AddArticleRequest;
+import me.kimyerin.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+
+
+}
